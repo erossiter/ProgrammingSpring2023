a29bd8ace2d7dbde2f3a52d7b1a29b30c5310782
Erin Rossiter <erossite@nd.edu>
2023-01-23 15:11:35 -0500

    day01 - introduction

---
+++ Day01-Intro/day01-review-basics-functions.R
@@ -0,0 +1,123 @@
+#############################
+# Functions
+#############################
+
+rm(list = ls())
+
+# Functions are objects that contain a list of instructions
+# We "call" a function to execute a command
+# A function has inputs and outputs
+
+# Example: f(x) = x^2
+#   - "f of x equals x squared"
+#   - x is the input; a placeholder
+#   - x^2 is the output; what to do with x
+# f(x=2) = 2^2 = 4
+
+
+# In R:
+f <- function(x){
+  return(x^2)
+}
+f(2)
+f(3)
+f(4)
+
+
+# Syntax must have
+# function_name <- function(var1, var2, ...){
+#   do whatever with var1, var2, ...
+#   return(output)
+#}
+
+
+# Example 1:
+
+# Need to read in our function, then you'll
+# see it in the environment panel
+sum_of_squares <- function(x, y){
+  return(x^2 + y^2)
+}
+
+# Then, we can use it with different values 
+sum_of_squares(x = 2, y = 3)
+sum_of_squares(x = 1, y = 1)
+
+# x and y are defined only in the "local environment" of the function
+# notice they are not in the global environment! We can't access them:
+x
+
+# Example 2:
+
+# This is equivalent to sum_of_squres()
+# but we break the operations out over multiple lines
+# NOTE:  "return statement" must be on the last line
+sum_of_squares2 <- function(x, y){
+  x_sq <- x^2
+  y_sq <- y^2
+  return(x_sq + y_sq) #
+}
+sum_of_squares2(x = 2, y = 3)
+
+
+# Built-in functions
+
+# NOTE: Why use functions? Any time you you use code repeatedly
+# to do a task, you should think of making it into a function.
+# That's why we have so many "built-in" functions to make life easier.
+
+data("USArrests")
+sum(USArrests$Assault)
+mean(USArrests$Assault, na.rm = T)
+boxplot(USArrests$Assault)
+print("hi")
+x <- 1
+y <- 2
+x + y # a function!
+mean(c(1,2,3))
+
+
+
+# Functions with vectors
+
+# Many functions have been "Vectorized"
+# meaning that they work on each element in the vector
+# or "elementwise" operations
+x <- 1:5 # vector
+x + 1 # + is a Vectorized function
+x^2
+y <- 1:5
+x*y
+
+# Other functions use the whole vector as its input
+x <- 1:5 
+mean(x)
+sum(x)
+length(x)
+summary(USArrests$Assault)
+max(USArrests$Assault)
+
+# Some functions evaluate each element
+is.na(USArrests$Assault)
+USArrests$Assault > 100
+
+# Some functions will create vectors as outputs
+rep(1, 5)
+seq(1, 21, by = 2) # Make the sequence 1 to 21 moving by increments of 2
+rep(seq(2, 20, by = 2), 2) # Repeat the pattern 2, 4, ... 20, twice
+rep(c(1,4), each = 3) # Repeat each value 3 times
+
+
+# WARNING!!  VECTOR RECYCLING!
+
+# When vectors have different lengths, the shorter one is extended
+# by repeating the vector.
+
+# This means two things:
+# 1. The vector lengths must be multiples of each other
+# 2. This is a very easy way to make a bad, bad mistake.
+
+# Example: say we wanted to add the murder and assault arrests
+# for each state... what is this code doing?
+USArrests$Murder[1:5] + USArrests$Assault
+
---
+++ Day01-Intro/day01-review-basics-vectors.R
@@ -0,0 +1,198 @@
+#############################
+# R Objects and Basic Syntax
+#############################
+
+## Note: We will be moving quickly.  I assume this is all very familiar.
+
+## Goals:
+# 1. Logistics of running R in RStudio
+# 2. Calculate simple arithmetic
+# 3. Assign a value to an object
+# 4. Types of Data
+# 5. Logical statements
+
+## Layout (Four windows):
+# Editor: Top left window. The .R or .Rmd "scripts" or files. Where you edit
+# Console: Bottom left window. Displays executed code and output.
+# Workspace: Top right window. All active objects, history of commands, etc.
+# Plots and help files: Bottom right window.
+
+## Comments
+# Anything that comes after a "#" sign.
+# Comments describe what the code is doing.
+
+## Running R code
+# To send a line of code to your console...
+# 1. Choose code
+# - put cursor on the line of code, or
+# - highlight exact code you want to run
+# 2. Run it
+# - (PC users) "control + enter"; (Mac users) "command + return", or
+# - highlight and hit "Run" at top of Editor (not recommended), or
+# - copy and paste the line of code (not recommended) 
+print("Hello world")
+print("My name is Erin")
+
+
+## Side note on code in .Rmd vs. .R files
+# Everything we're talking about applies when
+# the R code is inside an .Rmd code chunk!!!
+# You'll often work in a .R script instead,
+# so we want to be comfortable with both.
+
+
+## Features
+# - R is case sensitive (it is also spelling sensitive)
+# - R comes with many basic functions.
+# - Additional functionality are available in user-contributed packages
+#   that can be downloaded, loaded, and used.
+
+
+## R as a calculator
+# Basic arithmetic
+2 + 3  #addition
+2*3  # multiplication
+5/3 #division
+4 - 5 #subtraction
+3^3 # exponents
+625^(1/4) # More exponents
+log(10) # natural log
+
+# R follows the order of operations
+3+4/2 
+(3+4)/2 
+2*(3-4)+2
+2*(3-4)+2*(4 + 3)^(1/3)
+
+
+# R comes with a number of constants pre-stored that you can use
+6.25 # numbers
+pi # And a few others
+NA # Missing value
+NULL # Nothing.
+0/0 # NaN means "Not a number"
+NaN
+1/0 # Inf means infinity
+
+
+# Tip: Use the up and down arrows to access previously typed commands
+
+
+## Objects and named storage
+# The "<-" and "=" symbols are used to *assign* a variable to a value.
+# This is called "assignment", and these are "assignment operators"
+a <- 7 # after running this line, now R considers `a` as the number 7
+b = 8 # after running this line, now R considers `b` as the number 8
+# Print the values.
+print(a)
+print(b)
+# or simply
+a
+b
+
+# We might call `a` and `b` "objects" or "variables"
+
+## Using objects
+a + 3 # because `a` is a number, we can perform math
+a <- 4 # You can change the value an object takes  by executing this command
+a
+a + 3
+
+
+## Vectors:
+# - an ordered sequence of elements of the same data type
+# - like a "list", but we don't call it that,
+#   because list means something specific in R
+c(1,4,9) # built-in command is `c()`
+my_vec <- c(1,2,3)
+my_vec
+my_vec[3] # ordered
+my_vec*4 # math is "vectorized" -- applied to each element
+my_vec + 4
+my_vec - 4
+my_vec / 4
+
+
+## Beyond numbers
+# three data types: numeric, character, and logical	
+
+# numeric are numbers
+numeric_vector <- c(1,2,3,4,5)
+numeric_vector
+
+# strings
+# using quotation marks tells R we're working with this data type
+string_vector <- c("Erin", "Peitong", "Leonard")
+string_vector
+string_vector + 1 #Error! R knows it can only do math with certain object types
+
+# logicals
+# TRUE, FALSE, NA (must be all caps, that's who R knows what you mean)
+logical_vector <- c(TRUE, TRUE, FALSE, TRUE)
+logical_vector
+
+# Tip: Did you notice RStudio gives different object types different colors?
+
+## More operations using logicals
+# R knows how to compare objects as greater than, less than, or equal to each other
+4 < 5 #strictly less than
+4 <= 4 # less than or equal to
+4 > 5 # strictly greaeter than
+4 >= 5 # greater than or equal to
+4 == 4 # equal
+4 != 5 # not equal
+
+# More logical operators and truth tables
+# https://sites.millersville.edu/bikenaga/math-proof/truth-tables/truth-tables.html
+! 4 < 5 # read `!` as "NOT", it is negation
+a <- TRUE
+b <- TRUE
+c <- FALSE
+d <- FALSE
+
+# `&` means "AND"
+a & b # T AND T is T 
+a & c # T AND F is F
+c & d # F AND F is F
+
+# `|` the "pipe" means "OR"
+a | b # T OR T is T 
+a | c # T OR F is T 
+c | d # F OR F is F
+
+
+# Example putting things together
+total_votes <-  238307
+voting_age_population <- 496387
+turnout <- total_votes/voting_age_population
+turnout
+
+# Task: use R to see if turnout was 50% or more
+turnout >= .50
+
+# Tip:
+# - Objects are stored in the "global environment"
+# - You can easily see what you're working with
+# - These can be accessed at any time
+
+ls()
+
+# - You can also remove files from global environment
+
+hi <- "hi! :)"
+rm(hi)
+
+hi <- "hi! :)"
+bye <- "bye :("
+rm(list = c("hi", "bye"))
+
+# rm(list = ls()) #removes ~everything~ 
+
+# Tip: specific rules in a programming language is called "syntax"
+# - if-else statements in R have special syntax so R knows what to do
+# - note how it requires () and {}
+
+# Tip: comment multiple lines of code with command + shift + c (mac)
+
+
+
